8f2201a34dbb4c6490aea798cc0adab768e57c46
PierreTembremande <77484687+PierreTembremande@users.noreply.github.com>
2025-01-16 16:15:56 +0100

    Mise en place de la route "/". (#32)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {Home} from './Pages/Home'
+import { Routes, Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
